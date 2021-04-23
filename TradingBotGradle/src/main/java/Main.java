cd72dc6eb9619d1dd8ce7574e105da72b84de5f2
David Oberkalmsteiner <david.oberkalmsteiner@ost.ch>
2021-04-23 11:27:21 +0200

    initial Project

---
+++ TradingBotGradle/src/main/java/Main.java
@@ -0,0 +1,23 @@
+import Model.StopMode;
+import Model.ThreadDetails;
+import net.jacobpeterson.alpaca.AlpacaAPI;
+
+public class Main extends Thread {
+    public static void main(String[] args) throws InterruptedException {
+        AlpacaAPI alpacaAPI = new AlpacaAPI();
+        MarketData marketData = new MarketData(alpacaAPI);
+        MainController mainController = new MainController(marketData, alpacaAPI);
+        Thread thread = new Thread(mainController);
+
+        thread.start();
+
+        /*Thread.sleep(10000);
+        try{
+            mainController.stop(StopMode.fast, "TSLA");
+            System.out.println("Stopped TSLA fast");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }*/
+
+    }
+}
